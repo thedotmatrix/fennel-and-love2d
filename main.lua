cb6d596a073a461e4e9a33657ef96be3ee9825a6
Dot Matrix <transclosure@gmail.com>
2024-05-22 15:32:54 -0400

    rename wrap.fnl -> main.fnl
    now we have a congruent lua->fnl->cartridge (conf+main) pipeline

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local instal = {correlate=true, moduleName="lib.fennel"}
 local fennel = require("lib.fennel").install(instal)
-local make_love_searcher = function(env)
+local search = function(env) 
   return function(module_name)
     local path = module_name:gsub("%.", "/") .. ".fnl"
     if love.filesystem.getInfo(path) then
@@ -11,6 +11,7 @@ local make_love_searcher = function(env)
     end
   end
 end
+local make_love_searcher = function(env) return search(env) end
 table.insert(package.loaders, make_love_searcher(_G))
 table.insert(fennel["macro-searchers"], make_love_searcher("_COMPILER"))
-require("wrap")
+search(_G)("main")()
